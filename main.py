f42a6dd516d8f3979226df16c319f59a4fe3871a
Vladyslav Bychkovskyi <bickovskijvlad53@gmail.com>
2024-05-04 21:21:55 +0200

    Finall verssion

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
 import time
 print("Hello i am timer:)\nPlease write me a clock minutes and seconds:")
-a = int(1)
-while(a > 0):
-    clock = int(input("Clock:"))
-    minutes = int(input("Minutes"))
-    seconds = int(input("Seconds"))
-    if (clock > 0 and minutes > 0 and seconds > 0):
-        a = 0
-    else:
-        print("Please write me a correct clock minutes and seconds")
-        a = 1
-print("Timer is starting...")
-while(clock > 0):
-    while(minutes > 0):
-        while(seconds > 0):
-            time.sleep(1)
-            print(clock, ":", minutes, ":", seconds)
-            seconds -= 1
-        minutes -= 1
-        seconds = 59
-    clock -= 1
-    minutes = 59
-    seconds = 59
-    
-    
+hour = 0
+minute = 0
+second = 0
+while True:
+    try:
+        hour = int(input("Hours: "))
+        minute = int(input("Minutes: "))
+        second = int(input("Seconds: "))
+        break
+    except ValueError:
+        print("Please write a number!")
+        continue
+while True:
+    if (second > 60):
+        minute += second // 60
+        second %= 60
+    if (minute > 60):
+        hour += minute // 60
+        minute %= 60
+    if hour == 0 and minute == 0 and second == 0:
+        print("Time is over!")
+        break
+    print(f"{hour}:{minute}:{second}")
+    time.sleep(1)
+    second -= 1
+    if second < 0:
+        second = 59
+        minute -= 1
+    if minute < 0:
+        minute = 59
+        hour -= 1
+    if hour < 0:
+        hour = 0
